c53723b4db3a19bb9097c252d6169ac3c43a7acb
535220175_randy <randy@535220175@stu.untar.ac.id>
2025-06-13 12:22:40 +0700

    Setup Vercel configuration

--- BACKEND/api/index.js
+++ BACKEND/api/index.js
@@ -25,6 +25,7 @@ app.use(express.json());
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from Vercel Serverless!' });
 });
+app.use('/', authRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/checks', checkRoutes);
 
