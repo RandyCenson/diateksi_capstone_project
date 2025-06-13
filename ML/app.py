ed86f230d2cd696fcf2fa6ee6282cd3b8f7de7fe
Divano Rizqi Anandra <divanorizqi@gmail.com>
2025-06-13 16:27:07 +0700

    Update app.py

--- ML/app.py
+++ ML/app.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 import numpy as np
 import pandas as pd
 import joblib
@@ -18,7 +17,6 @@ ORDERED_COLUMNS = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', '
                    'BMI', 'DiabetesPedigreeFunction', 'Age', 'dummy_0']
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/')
 def home():
